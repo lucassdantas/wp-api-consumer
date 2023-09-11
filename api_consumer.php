568a94a3453ee2fd772c08e3065a92074be6f96c
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-11 16:36:02 -0300

    change plugin uri

--- api_consumer.php
+++ api_consumer.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Plugin Name: Api Consumer
-* Plugin URI: https://github.com/lucassdantas/consultoria_presencial_plugin_wp.git
+* Plugin URI: https://github.com/lucassdantas/wp-api-consumer.git
 * Description: Plugin to send elementor form to API
 * Version: 0.1
 * Author: R&D Marketing Digital
