7059e7165abdd972ba39a944836a1507cb4ee679
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-11 16:35:27 -0300

    first commit

---
+++ api_consumer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Plugin Name: Api Consumer
+* Plugin URI: https://github.com/lucassdantas/consultoria_presencial_plugin_wp.git
+* Description: Plugin to send elementor form to API
+* Version: 0.1
+* Author: R&D Marketing Digital
+* Author URI: https://rdmarketing.com.br/
+**/
+
+defined('ABSPATH') or die();
+if(!function_exists('add_action')){
+    die;
+}
