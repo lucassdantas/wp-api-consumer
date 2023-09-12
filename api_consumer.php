3de7684d59be4e2b5eaab59f0a1133c409199435
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-12 14:54:52 -0300

    api request working

--- api_consumer.php
+++ api_consumer.php
@@ -15,38 +15,51 @@ if(!function_exists('add_action')){
     die;
 }
 add_action( 'elementor_pro/forms/new_record', function( $record, $ajax_handler ) {
-	//make sure its our form
-	$form_name = $record->get_form_settings( 'form_name' );
-	if ( 'criar_conta' !== $form_name ) {
-		return;
-	}
-	//normalize the fields
-	$raw_fields = $record->get( 'fields' );
-	$fields = [];
-	foreach ( $raw_fields as $id => $field ) {
-		$fields[ $id ] = $field['value'];
-	}
-	wp_remote_post(
-		'https://prisma.dev.br/api/signup',
-		array(
-			'method'      => 'POST',
-			'timeout'     => 45,
-			'redirection' => 5,
-			'httpversion' => '1.0',
-			'blocking'    => true,
-			'headers'     => array(
-				'Content-Type' => 'application/json'
-			),
-			'body'        => array(
-				'userName' => 'bob',
-				'email' => '1234xyz',
-				'confirmEmail' => '1234xyz',
-				'password' => '1234xyz',
-				'confirmPassword' => '1234xyz',
-				'photoBase64' => '1234xyz',
-				'cellPhone' => '1234xyz'
-			),
-		)
-	);
-	$ajax_handler->data['output'] = 'INFORM_02 Output is: ' . $fields['email'];
+    // Certifique-se de que é o nosso formulário
+    $form_name = $record->get_form_settings( 'form_name' );
+    if ( 'criar_conta' !== $form_name ) {
+        return;
+    }
+    
+    // Normalize os campos
+    $raw_fields = $record->get( 'fields' );
+    $fields = [];
+    foreach ( $raw_fields as $id => $field ) {
+        $fields[ $id ] = $field['value'];
+    }
+
+    // Prepare os dados para a solicitação
+    $request_data = array(
+        'userName' => $fields['userName'],
+        'email' => $fields['email'],
+        'confirmEmail' => $fields['confirmEmail'],
+        'password' => $fields['password'],
+        'confirmPassword' => $fields['confirmPassword'],
+        'photoBase64' => $fields['photoBase64'],
+        'cellPhone' => $fields['cellPhone']
+    );
+
+    // Realize a solicitação POST
+    $response = wp_remote_post(
+        'https://prisma.dev.br/api/signup',
+        array(
+            'method'      => 'POST',
+            'timeout'     => 45,
+            'redirection' => 5,
+            'httpversion' => '1.0',
+            'blocking'    => true,
+            'headers'     => array(
+                'Content-Type' => 'application/json'
+            ),
+            'body'        => wp_json_encode( $request_data ),
+        )
+    );
+
+    // Verifique se houve um erro na solicitação
+    if( is_wp_error( $response ) ) {
+        $ajax_handler->data['output'] = $response->get_error_message();
+    } else {
+        // A resposta está no corpo (body) da resposta
+        $ajax_handler->data['output'] = wp_remote_retrieve_body( $response );
+    }
 }, 10, 2);
