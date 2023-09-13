1ca41131f28ec37282d7c5d1ab8d48c3d5c6c2db
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-13 08:21:09 -0300

    add link to redirect after successfuly submit

--- script_to_bottom_form.js
+++ script_to_bottom_form.js
@@ -1,5 +1,3 @@
-<script>
-//The event is submit_success so you can catch it for example:
 jQuery( document ).ready(function( $ ){
     $( document ).on('submit_success', function( event, response ){
         if ( response.data.output ) {
@@ -8,23 +6,14 @@ jQuery( document ).ready(function( $ ){
             if(data.status == 200){
                 document.querySelector('#formAccountMessage').innerHTML = 'Conta criada com sucesso! Em Breve, será redirecioado.'
                 setTimeout(() => {
-                    location.href = ''
+                    location.href = 'https://prisma.devrdmarketing.com/obrigado/'
                 }, 3000)
-            }
-            if(data.status == 400){
+            }else if(data.status == 400){
                 document.querySelector('#formAccountMessage').innerHTML = 'Os dados estão incorretos ou a conta já existe'
             }
             else{
 
             }
-            /*
-            {
-                "status":200,
-                "message":"User teste03@rdmarketing.com.br - teste03@rdmarketing.com.br successfully inserted"
-            }
-            */
-            console.log( response.data.output  );
         }
     });
 });
-</script>
